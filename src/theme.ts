d13e5aa4dbe2c693ac55687abb7da1453888a516
Lucas Ferreira <lucasferreiraek@gmail.com>
2024-08-11 14:50:56 -0300

    feat: define color pallete

--- src/theme.ts
+++ src/theme.ts
@@ -1,13 +1,29 @@
-import { createTheme } from '@mui/material/styles';
+import { alpha, createTheme, getContrastRatio } from '@mui/material/styles';
+
+const darkGray = "#1C1C1C"
+const ghost = "#F2F2F2"
+const deepPurple = "#4527A0"
 
 const theme = createTheme({
   palette: {
     primary: {
-      main: "#232323",
+      main: darkGray,
+      light: alpha(darkGray, 0.5),
+      dark: alpha(darkGray, 0.9),
+      contrastText: getContrastRatio(darkGray, "#fff") > 4.5 ? '#fff' : '#111'
     },
     secondary: {
-      main: "#dd4141",
+      main: ghost,
+      light: alpha(ghost, 0.5),
+      dark: alpha(ghost, 0.9),
+      contrastText: darkGray
     },
+    info: {
+      main: deepPurple,
+      light: alpha(deepPurple, 0.5),
+      dark: alpha(deepPurple, 0.9),
+      contrastText: ghost
+    }
   },
   typography: {
     fontFamily: "JetBrains Mono",
