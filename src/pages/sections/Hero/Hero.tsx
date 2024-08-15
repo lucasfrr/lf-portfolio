8c209801e49847c172c9820259663bd1a69bcf17
Lucas Ferreira <lucasferreiraek@gmail.com>
2024-08-15 16:36:09 -0300

    feat: open dialog to donwload cv when click on button download

--- src/pages/sections/Hero/Hero.tsx
+++ src/pages/sections/Hero/Hero.tsx
@@ -1,9 +1,11 @@
-import { Box, Container, Grid, styled, Typography } from "@mui/material"
+import { Box, Button, Container, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, Grid, Link, Slide, styled, Typography, useMediaQuery, useTheme } from "@mui/material"
 import DownloadIcon from '@mui/icons-material/Download';
 import EmailIcon from '@mui/icons-material/Email';
 
 import Avatar from "../../../assets/images/avatar.jpg"
 import LFButton from "../../../components/LFButton/LFButton";
+import { TransitionProps } from "@mui/material/transitions";
+import React from "react";
 
 const Hero = () => {
 
@@ -39,9 +41,27 @@ const Hero = () => {
         transition: "border-color 0.5s, background-color 0.5s",
         '&:hover': {
             backgroundColor: theme.palette.info.main,
-            border: `1px solid ${theme.palette.info.dark}`
+            border: `2px solid ${theme.palette.info.dark}`
         }
     }))
+
+    const Transition = React.forwardRef(function Transition(
+        props: TransitionProps & {
+            children: React.ReactElement<any, any>;
+        },
+        ref: React.Ref<unknown>,
+    ) {
+        return <Slide direction="up" ref={ref} {...props} />
+    })
+
+    const [open, setOpen] = React.useState(false)
+    const handleClickOpen = () => {setOpen(true)}
+    const handleClose = () => {setOpen(false)}
+    const theme = useTheme()
+    const fullScreen = useMediaQuery(theme.breakpoints.down("md"))
+
+    const cvPortuguese = "https://docs.google.com/document/d/18IP8h8ai94lE2Gk71Oex0aAos9mTB7UybGOZn8WpOxY/pub"
+    const cvEnglish = "https://docs.google.com/document/d/1asqcqWN2-J26qBHVMXL4_-VTLU_9TH6hZt9AwWG58MM/pub"
 
     return (
         <>
@@ -59,10 +79,35 @@ const Hero = () => {
                             
                             <Grid container display="flex" justifyContent="center" spacing={3} pt={8}>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
-                                    <LFButton onClick={() => window.alert("Download")}>
+                                    <LFButton onClick={handleClickOpen}>
                                         <DownloadIcon />
                                         <Typography>Download CV</Typography>
                                     </LFButton>
+                                    <React.Fragment>
+                                        <Dialog
+                                            fullScreen={fullScreen}
+                                            open={open}
+                                            TransitionComponent={Transition}
+                                            keepMounted
+                                            onClose={handleClose}
+                                            aria-describedby="alert-dialog-cv-download"
+                                        >
+                                            <DialogContent>
+                                                <DialogTitle>{"Escolha o idioma"}</DialogTitle>
+                                                <DialogContentText id="alert-dialog-cv-download">
+                                                    <Typography textAlign="center">
+                                                        <Link href={cvPortuguese}>CV em português</Link>
+                                                    </Typography>
+                                                    <Typography textAlign="center">
+                                                        <Link href={cvEnglish}>CV in english</Link>
+                                                    </Typography>
+                                                </DialogContentText>
+                                            </DialogContent>
+                                            <DialogActions>
+                                                <Button onClick={handleClose}>Fechar</Button>
+                                            </DialogActions>
+                                        </Dialog>
+                                    </React.Fragment>
                                 </Grid>
                                 <Grid item xs={12} md={4} display="flex" justifyContent="center">
                                     <LFButton onClick={() => window.alert("email")}>
